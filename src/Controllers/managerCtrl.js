ce08e4e3c59d54fafeba2aa13348c51954681ca3
Michael Gassert <michael.gassert@wwt.com>
2016-07-27 16:14:34 -0500

    Implemented a post for managers

--- src/Controllers/managerCtrl.js
+++ src/Controllers/managerCtrl.js
@@ -1,20 +1,20 @@
 var Manager = require('./../Models/managerModel');
 
-exports.list = function (req,res) {
-	console.log(Manager.find())
-    Manager.find({},function(err,Manager) {
-    	if(err)return console.error(err);
+exports.list = function (req, res) {
+    console.log(Manager.find())
+    Manager.find({}, function (err, Manager) {
+        if (err) return console.error(err);
         console.log(Manager);
         res.json(Manager);
     })
 }
 
-// exports.save = function (req,res) {
-// 	console.log(req);
-// 	var newManager = new managerModel;
-
-// 	Manager.save(function(err,Manager){
-// 		console.log('post')
-// 		res.json(Manager);
-// 	})
-// }
+exports.create = function (req, res) {
+    try {
+        var newManager = new Manager(req.body);
+        newManager.save();
+        res.json(newManager);
+    } catch (err) {
+        res.json(err);
+    }
+}
--- src/Routes/managerRoutes.js
+++ src/Routes/managerRoutes.js
@@ -3,5 +3,5 @@ var managerCtrl = require('./../Controllers/managerCtrl');
 module.exports = function(app) {
 
     app.route('/api/managers').get(managerCtrl.list);
-    // app.route('/api/managers').post(managerCtrl.save);
+    app.route('/api/managers').post(managerCtrl.create);
 };
